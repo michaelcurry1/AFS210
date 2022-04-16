8b2d766b962019acfbe44aa5a09d40f9d4120f69
Your usernamemichaelcurry1 <michael.curry@bryanuniversity.edu>
2022-04-16 19:28:08 -0400

    1st

--- week4/hashing.py
+++ week4/hashing.py
@@ -5,16 +5,45 @@ class HashTable:
         self.data = [None] * self.size
 
     def hashfunction(self,key):
-        # Insert your hashing function code
+        # Insert your hashing function code0
+        return key % self.size
 
     def rehash(self,key):
         # Insert your secondary hashing function code
+        return key // self.size
 
     def put(self,key,data):
         # Insert your code here to store key and data 
+        hashvalue = self.hashfunction(key)
+        if self.data[hashvalue] == None:
+            self.slots[hashvalue] = key
+            self.data[hashvalue] = data
+        else:
+            if self.slots[hashvalue] == key:
+                self.data[hashvalue] = data
+            else:
+                nextslot = self.rehash(key)
+                if self.data[nextslot] == None:
+                    self.data[nextslot] = data
+                    self.slots[nextslot] = key
+                else:
+                        print("Unresolved Collision")
 
     def get(self,key):
         # Insert your code here to get data by key
+        data = None
+        position = self.hashfunction(key)
+        # print("slots",self.slots)
+        # print("slot in positition",self.slots[position])
+        if self.slots[position] == key:
+            data = self.data[position]
+        # else:
+        #     position = self.rehash(key)
+        #     if self.slots[position] ==key:
+        #         data = self.data[position]
+
+        return data
+
 
     def __getitem__ (self,key):
         return self.get(key)
@@ -25,7 +54,18 @@ class HashTable:
 
 H = HashTable()
 H[69] = 'A'
+H[66] = 'B'
+H[80] = 'C'
+H[35] = 'D'
+H[18] = 'E'
+H[52] = 'F'
+H[89] = 'G'
+H[70] = 'H'
+H[12] = 'I'
 # Store remaining input data
+print(H.slots)
+print(H.data)
+print(H[52])
 
 # print the slot values
 
